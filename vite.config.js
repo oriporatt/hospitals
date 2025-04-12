41c4b8b13a0d64e321f8e4cef4ac897c139ebe6e
oriporatt <oriporatt@gmail.com>
2025-04-12 18:35:05 +0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hospitals/', // <-- set to your GitHub repo name!
+
   plugins: [react()],
 })
